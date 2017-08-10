d88660a7ccebeaac8fa82335507c54fed3f9af04
15004557 <H0peyur3gd7>
2017-08-10 12:37:45 +0800

    With image

--- app/src/main/java/democooldrawer/android/myapplicationdev/com/p12_mydatabook/AboutUsActivity.java
+++ app/src/main/java/democooldrawer/android/myapplicationdev/com/p12_mydatabook/AboutUsActivity.java
@@ -16,7 +16,6 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-
         ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
 
